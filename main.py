4fc2cbad1ac64e85112d0a9d116b024f0c655f3e
zhuoyangjiang <70708249+zhuoyangjiang@users.noreply.github.com>
2020-09-17 22:02:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,15 +22,15 @@ def getGradePoint(grade):
 
 def run():
     gradepoint1 = getGradePoint(input("Enter your course 1 letter grade: "))
-    credit1 = int(input("Enter your course 1 credit:"))
+    credit1 = int(input("Enter your course 1 credit: "))
     print(f"Grade point for course 1 is: {gradepoint1}")
 
     gradepoint2 = getGradePoint(input("Enter your course 2 letter grade: "))
-    credit2 = int(input("Enter your course 2 credit:"))
+    credit2 = int(input("Enter your course 2 credit: "))
     print(f"Grade point for course 2 is: {gradepoint2}")
 
     gradepoint3 = getGradePoint(input("Enter your course 3 letter grade: "))
-    credit3 = int(input("Enter your course 3 credit:"))
+    credit3 = int(input("Enter your course 3 credit: "))
     print(f"Grade point for course 3 is: {gradepoint3}")
 
     GPA = (gradepoint1 * credit1 + gradepoint2 * credit2 + gradepoint3 * credit3) / (credit1 + credit2 + credit3) 
